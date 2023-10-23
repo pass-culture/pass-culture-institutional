a572d558c4ebf75d756a3545a3bf9c88c7d0e1c3
Hugo Voisin <hugov@bam.tech>
2023-10-23 17:41:33 +0200

    test: add behavior test, clicking on a checkbox

--- frontend/src/pages/home.test.tsx
+++ frontend/src/pages/home.test.tsx
@@ -3,7 +3,7 @@ import { describe, expect, it } from 'vitest'
 import { axe } from 'vitest-axe'
 
 import Home from './index'
-import { render } from '@/tests'
+import { fireEvent, render, screen } from '@/tests'
 
 describe('Home page', () => {
   it('should pass axe accessibility tests', async () => {
@@ -16,5 +16,16 @@ describe('Home page', () => {
     const { container } = render(<Home />)
 
     expect(container).toMatchSnapshot()
+  })
+
+  it('should select the checkbox when clicking on its label', () => {
+    render(<Home />)
+
+    const checkbox = screen.getByRole('checkbox')
+    const label = screen.getByText('Checkbox à cocher')
+
+    fireEvent.click(label)
+
+    expect(checkbox).toHaveProperty('checked', true)
   })
 })
