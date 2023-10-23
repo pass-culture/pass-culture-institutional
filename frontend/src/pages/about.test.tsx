5bc8507b5648856934ea7c3f3d2ce4f664b5b1cb
Hugo Voisin <hugov@bam.tech>
2023-10-23 16:35:39 +0200

    test: use custom render in tests

--- frontend/src/pages/about.test.tsx
+++ frontend/src/pages/about.test.tsx
@@ -1,19 +1,13 @@
 import React from 'react'
-import { render } from '@testing-library/react'
-import { ThemeProvider } from 'styled-components'
 import { describe, expect, it } from 'vitest'
 import { axe } from 'vitest-axe'
 
 import About from './about'
-import { theme } from '@/theme/theme'
+import { render } from '@/tests'
 
 describe('About page', () => {
   it('should pass axe accessibility tests', async () => {
-    const { container } = render(
-      <ThemeProvider theme={theme}>
-        <About />
-      </ThemeProvider>
-    )
+    const { container } = render(<About />)
 
     expect(await axe(container)).toHaveNoViolations()
   })
--- frontend/src/pages/home.test.tsx
+++ frontend/src/pages/home.test.tsx
@@ -1,19 +1,13 @@
 import React from 'react'
-import { render } from '@testing-library/react'
-import { ThemeProvider } from 'styled-components'
 import { describe, expect, it } from 'vitest'
 import { axe } from 'vitest-axe'
 
 import Home from './index'
-import { theme } from '@/theme/theme'
+import { render } from '@/tests'
 
 describe('Home page', () => {
   it('should pass axe accessibility tests', async () => {
-    const { container } = render(
-      <ThemeProvider theme={theme}>
-        <Home />
-      </ThemeProvider>
-    )
+    const { container } = render(<Home />)
 
     expect(await axe(container)).toHaveNoViolations()
   })
