88a107454818a31fdbf564f72e0dfeeb0c7952e9
Christopher Gerrard <christopher.gerrard@passculture.app>
2023-10-26 15:54:21 +0200

    removed useless comment

--- backend/tests/app.test.ts
+++ backend/tests/app.test.ts
@@ -3,7 +3,7 @@ import { setupStrapi, cleanupStrapi } from "./helpers/strapi";
 import { Strapi } from "@strapi/strapi";
 import { describe, beforeAll, afterAll, it, expect } from "@jest/globals";
 
-let strapiInstance: Strapi; // Replace 'any' with a more specific type if possible
+let strapiInstance: Strapi;
 
 beforeAll(async () => {
   strapiInstance = await setupStrapi();
