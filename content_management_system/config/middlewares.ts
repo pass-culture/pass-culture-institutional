cd4395c4ff820b911698d08083ffddff0c88b1c0
Christopher Gerrard <christopher.gerrard@passculture.app>
2023-12-19 17:24:05 +0100

    added strapi CORS policy

--- content_management_system/config/middlewares.ts
+++ content_management_system/config/middlewares.ts
@@ -1,12 +1,23 @@
 export default [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::errors",
+  "strapi::security",
+  {
+    name: "strapi::cors",
+    config: {
+      headers: "*",
+      origin: [
+        "http://localhost:1337",
+        "http://localhost:3000",
+        "https://siteinstit.testing.passculture.team",
+        "https://siteinstit.staging.passculture.team",
+      ],
+    },
+  },
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
--- public_website/src/utils/fetchAPI.ts
+++ public_website/src/utils/fetchAPI.ts
@@ -14,12 +14,25 @@ export async function fetchAPI<T>(path: string) {
     const requestUrl = `${getStrapiURL(apiPath)}`
 
     const response = await fetch(requestUrl, mergedOptions)
-    const data: HttpResponse<T> = await response.json()
-    return data
+    if (!response.ok) {
+      throw new Error(`Server returned a non-OK status: ${response.status}`)
+    }
+
+    const contentType = response.headers.get('Content-Type') || ''
+
+    if (contentType.includes('application/json')) {
+      const data: HttpResponse<T> = await response.json()
+      return data
+    } else {
+      // Handle non-JSON responses (e.g., display an error message)
+      throw new Error(
+        `Unexpected response. Content type received: ${contentType}`
+      )
+    }
   } catch (error) {
     console.error(error)
     throw new Error(
-      `Please check if your server is running and you set all the required tokens.`
+      `Please check if your server is running and you set all the required tokens. ${error}`
     )
   }
 }
