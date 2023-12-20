3b04434a5a220b1c724ee5a0a3612506cb3e3f53
Christopher Gerrard <christopher.gerrard@passculture.app>
2023-12-20 11:41:55 +0100

    google auth for next

---
+++ public_website/src/utils/googleIAPAuth.ts
@@ -0,0 +1,23 @@
+const protectedUrl = process.env['NEXT_PUBLIC_STRAPI_API_URL']
+const targetAudience =
+  'http://427121120704-9qg83t8p14i1ncdbv8fi44nfu2te28dm.apps.googleusercontent.com/'
+
+import { GoogleAuth } from 'google-auth-library'
+// const { GoogleAuth } = require('google-auth-library')
+const auth = new GoogleAuth()
+
+export async function googleIAPAuth() {
+  try {
+    // eslint-disable-next-line no-console
+    console.info(
+      `request IAP ${protectedUrl} with target audience ${targetAudience}`
+    )
+    const client = await auth.getIdTokenClient(targetAudience)
+    const res = await client.request({ url: protectedUrl })
+
+    return res.data
+  } catch (error) {
+    console.error(error)
+    process.exitCode = 1
+  }
+}
