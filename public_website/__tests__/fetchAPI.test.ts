ec8f1d5597ce5c9809a2ad2d4f41df154d55a52d
Christopher Gerrard <christopher.gerrard@passculture.app>
2023-12-27 11:07:44 +0100

    Changed tests to use MSW

--- public_website/__tests__/fetchAPI.test.ts
+++ public_website/__tests__/fetchAPI.test.ts
@@ -1,7 +1,44 @@
-import fetchMock from 'fetch-mock'
-import { describe, expect, it } from 'vitest'
+import { http, HttpResponse } from 'msw'
+import { setupServer } from 'msw/node'
+import {
+  afterAll,
+  afterEach,
+  beforeAll,
+  describe,
+  expect,
+  it,
+  vi,
+} from 'vitest'
 
 import { fetchAPI } from '@/utils/fetchAPI'
+
+const mockFetch = vi.spyOn(global, 'fetch')
+const server = setupServer(
+  http.get('http://localhost:1337/api/test', () => {
+    return HttpResponse.json({ data: 'dummy data' })
+  })
+)
+
+const respondWith = async (
+  body: unknown,
+  status = 200,
+  statusText?: string,
+  headers?: HeadersInit
+): Promise<Response> => {
+  return new Response(JSON.stringify(body), {
+    headers: {
+      'content-type': 'application/json',
+      ...headers,
+    },
+    status,
+    statusText,
+  })
+}
+
+beforeAll(() => server.listen())
+afterEach(() => server.resetHandlers())
+afterAll(() => server.close())
+const mockSuccessfulResponse = { data: 'dummy data' }
 
 describe('fetchAPI', async () => {
   it('should fail if no token is found', async () => {
@@ -11,29 +48,25 @@ describe('fetchAPI', async () => {
   })
   it('should pass if token is found', async () => {
     process.env = { ...process.env, ID_TOKEN: 'your_dummy_token' }
-    fetchMock.get('http://localhost:1337/api/test', { data: 'dummy data' })
-
+    mockFetch.mockResolvedValueOnce(await respondWith(mockSuccessfulResponse))
     const response = await fetchAPI('/test')
-    expect(response).toEqual({ data: 'dummy data' })
+    await expect(response).toEqual(mockSuccessfulResponse)
   })
 
   it('should fail if response is not ok', async () => {
     process.env = { ...process.env, ID_TOKEN: 'your_dummy_token' }
-    fetchMock.get('http://localhost:1337/api/test2', 500)
-
-    await expect(fetchAPI('/test2')).rejects.toThrow(
+    mockFetch.mockResolvedValueOnce(await respondWith({}, 500))
+    await expect(fetchAPI('/test')).rejects.toThrow(
       'Server returned a non-OK status: 500'
     )
   })
 
   it('should fail if response is not json', async () => {
     process.env = { ...process.env, ID_TOKEN: 'your_dummy_token' }
-
-    fetchMock.get('http://localhost:1337/api/test3', {
-      headers: { 'Content-Type': 'text/html' },
-    })
-
-    await expect(fetchAPI('/test3')).rejects.toThrow(
+    mockFetch.mockResolvedValueOnce(
+      await respondWith({}, 200, 'OK', { 'content-type': 'text/html' })
+    )
+    await expect(fetchAPI('/test')).rejects.toThrow(
       'Unexpected response. Content type received: text/html'
     )
   })
@@ -41,9 +74,13 @@ describe('fetchAPI', async () => {
   it('contentType should be false if no header is found', async () => {
     process.env = { ...process.env, ID_TOKEN: 'your_dummy_token' }
 
-    fetchMock.get('http://localhost:1337/api/test4', 200)
+    mockFetch.mockResolvedValueOnce(
+      await respondWith(mockSuccessfulResponse, 200, 'OK', {
+        'content-type': '',
+      })
+    )
 
-    await expect(fetchAPI('/test4')).rejects.toThrow(
+    await expect(fetchAPI('/test')).rejects.toThrow(
       'Unexpected response. Content type received: '
     )
   })
