0edebce0e600439595e6fcd6d2f96b95d1b506dc
Christopher Gerrard <christopher.gerrard@passculture.app>
2024-01-22 15:00:29 +0100

    made tag route public

--- content_management_system/src/api/active-playlist-tag/routes/active-playlist-tag.ts
+++ content_management_system/src/api/active-playlist-tag/routes/active-playlist-tag.ts
@@ -2,6 +2,18 @@
  * active-playlist-tag router
  */
 
-import { factories } from '@strapi/strapi';
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreRouter('api::active-playlist-tag.active-playlist-tag');
+export default factories.createCoreRouter(
+  "api::active-playlist-tag.active-playlist-tag",
+  {
+    config: {
+      find: {
+        auth: false,
+      },
+      create: {
+        auth: false,
+      },
+    },
+  }
+);
