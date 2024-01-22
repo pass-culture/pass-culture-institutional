c1b2ebbf989bad2a3fd7dd5fd56e4398b8dbee64
Christopher Gerrard <christopher.gerrard@passculture.app>
2024-01-22 14:59:38 +0100

    Added tag to seed

--- content_management_system/src/index.ts
+++ content_management_system/src/index.ts
@@ -15,31 +15,68 @@ export default {
    * run jobs, or perform some special logic.
    */
   async bootstrap({ strapi }) {
-    const existingRestaurants = await strapi.entityService.findMany(
-      "api::restaurant.restaurant",
-      {
-        filters: {
-          name: "Seed Restaurant 1",
-        },
-      }
-    );
-    if (existingRestaurants.length > 0) {
-      // eslint-disable-next-line no-console
-      console.log("Seed restaurants already exist", existingRestaurants);
-      return;
-    }
-
-    for (let index = 0; index < 3; index++) {
-      // eslint-disable-next-line no-console
-      console.log(`Creating Seed Restaurant ${index + 1}`);
-
-      await strapi.entityService.create("api::restaurant.restaurant", {
-        data: {
-          name: `Seed Restaurant ${index + 1}`,
-          description: `Seed description ${index + 1}`,
-          publishedAt: new Date(),
-        },
-      });
-    }
+    await createSeedRestaurant(strapi);
+    await createSeedActivePlaylistTag(strapi);
   },
 };
+
+async function createSeedRestaurant(strapi) {
+  const existingRestaurants = await strapi.entityService.findMany(
+    "api::restaurant.restaurant",
+    {
+      filters: {
+        name: "Seed Restaurant 1",
+      },
+    }
+  );
+  if (existingRestaurants.length > 0) {
+    // eslint-disable-next-line no-console
+    console.log("Seed restaurants already exist", existingRestaurants);
+    return;
+  }
+
+  for (let index = 0; index < 3; index++) {
+    // eslint-disable-next-line no-console
+    console.log(`Creating Seed Restaurant ${index + 1}`);
+
+    await strapi.entityService.create("api::restaurant.restaurant", {
+      data: {
+        name: `Seed Restaurant ${index + 1}`,
+        description: `Seed description ${index + 1}`,
+        publishedAt: new Date(),
+      },
+    });
+  }
+}
+
+async function createSeedActivePlaylistTag(strapi) {
+  const existingActivePlaylistTags = await strapi.entityService.findMany(
+    "api::active-playlist-tag.active-playlist-tag",
+    {
+      filters: {
+        name: "Bons_plans_du_moment",
+      },
+    }
+  );
+  if (existingActivePlaylistTags.length > 0) {
+    // eslint-disable-next-line no-console
+    console.log(
+      "Seed active playlist tags already exist",
+      existingActivePlaylistTags
+    );
+    return;
+  }
+
+  // eslint-disable-next-line no-console
+  console.log(`Creating Seed Active Playlist Tag "Bons_plans_du_moment"`);
+
+  await strapi.entityService.create(
+    "api::active-playlist-tag.active-playlist-tag",
+    {
+      data: {
+        name: "Bons_plans_du_moment",
+        publishedAt: new Date(),
+      },
+    }
+  );
+}
