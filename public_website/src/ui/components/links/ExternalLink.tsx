37c3d745eb1587353ce3490037488017ca73dd0a
Quentin <quentin@bien-fonde.com>
2024-02-12 14:19:04 +0100

    revert colors naming

--- public_website/src/ui/components/links/ExternalLink.tsx
+++ public_website/src/ui/components/links/ExternalLink.tsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
 export const ExternalLink = styled.a(({ theme }) => ({
-  color: theme.colors.hardBlue,
+  color: theme.colors.secondary,
   textDecoration: 'none',
   ['&:hover, :focus, :active']: {
     textDecoration: 'underline',
