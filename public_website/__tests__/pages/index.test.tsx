ef9ac6eed126b196249aa0ac1da4205e5367dbd3
Quentin <quentin@bien-fonde.com>
2024-02-22 13:23:46 +0100

    add env variables in tests

--- public_website/__tests__/pages/index.test.tsx
+++ public_website/__tests__/pages/index.test.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
-import { describe, expect, it } from 'vitest'
+import { beforeEach, describe, expect, it } from 'vitest'
 import { axe } from 'vitest-axe'
 
 import { act, render } from '..'
 import Home, { getStaticProps } from '@/pages'
 
 describe('Home page', () => {
+  beforeEach(() => {
+    process.env = {
+      ...process.env,
+      ID_TOKEN: 'dummy_token',
+      BACKEND_API_URL: 'http://dummy_localhost:5001',
+    }
+  })
+
   it('should pass accessibility tests', async () => {
     const { props } = await getStaticProps()
     const { container } = render(
