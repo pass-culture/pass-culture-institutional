71e4e15c8b49483208ff29427a16593fa2f527d2
Quentin <quentin@bien-fonde.com>
2024-02-29 12:29:23 +0100

    remove shadow on emoji

--- public_website/src/theme/theme.ts
+++ public_website/src/theme/theme.ts
@@ -91,7 +91,6 @@ export const theme = {
       lineHeight: '1',
       fontSize: FontSizes['8XL'],
       fontWeight: FontWeights.BOLD,
-      // boxShadow: `${Shadows.STICKER} ${Colors.LIGHT_GRAY}`,
     },
     body: {
       desktop: {
