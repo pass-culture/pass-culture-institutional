f3aac3e097a1b3a9547abffe666fb1168afcc6f2
Louis GERARD <louis.gerard@passculture.app>
2024-02-29 15:10:37 +0100

    feat: add s3 upload provider to strapi

--- content_management_system/config/plugins.ts
+++ content_management_system/config/plugins.ts
@@ -9,4 +9,19 @@ export default ({ env }) => ({
       branch: 'main',
     },
   },
+  'upload': {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        accessKeyId: env('S3_ACCESS_ID'),
+        secretAccessKey: env('S3_SECRET_ID'),
+        endpoint: `https://${env('S3_ENDPOINT_URL')}`,
+        region: "auto",
+        signatureVersion: "v4",
+        params: {
+          Bucket: env('S3_BUCKET_NAME'),
+        },
+      },
+    },
+  },
 });
