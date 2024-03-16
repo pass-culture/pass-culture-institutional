3e0e804170dd2971d3d50b3b029462b825142ea1
Amadou Affo <62760390+AffoAmadou@users.noreply.github.com>
2024-03-16 13:42:20 +0100

    block render

--- public_website/src/lib/BlockRenderer.tsx
+++ public_website/src/lib/BlockRenderer.tsx
@@ -1,14 +1,22 @@
 import React, { ComponentProps } from 'react'
 
 import { CenteredText } from './blocks/CenteredText'
+import { ExperienceVideo } from './blocks/ExperienceVideo'
+import { ExperienceVideoCarousel } from './blocks/experienceVideoCarousel/experienceVideoCarousel'
 import { Header } from './blocks/Header'
+import { KeyNumber } from './blocks/Keynumber'
+import { KeyNumberCarousel } from './blocks/keyNumberCarousel/keyNumberCarousel'
 import { LatestNews } from './blocks/LatestNews'
+import { LogoCarousel } from './blocks/logoCarousel/logoCarousel'
+import { LogoCarouselSlide } from './blocks/logoCarousel/logoCarouselSlide'
+import { Logos } from './blocks/Logos'
 import { PushCTA } from './blocks/PushCTA'
 import { SimpleText } from './blocks/SimpleText'
 import { SocialMedia } from './blocks/SocialMedia'
+import { Testimonies } from './blocks/Testimonies'
+import { TestimonyCarouselSlide } from './blocks/testimonyCaousel/testimonyCarouselSlide'
 import { VerticalCarousel } from './blocks/verticalCarousel/VerticalCarousel'
 import { UnkwnownBlock } from './UnknownBlock'
-
 export interface Block {
   __component: string
 }
@@ -29,6 +37,15 @@ const COMPONENTS: Record<
   'block.social-media': SocialMedia,
   'block.latest-news': LatestNews,
   'block.vertical-carousel': VerticalCarousel,
+  'block.experience-video': ExperienceVideo,
+  'block.key-number': KeyNumber,
+  'block.logos': Logos,
+  'block.testimonies': Testimonies,
+  'block.experience-video-carousel': ExperienceVideoCarousel,
+  'block.key-number-carousel': KeyNumberCarousel,
+  'block.logo-carousel': LogoCarousel,
+  'block.logo-carousel-slide': LogoCarouselSlide,
+  'block.testimony-carousel': TestimonyCarouselSlide,
 }
 
 export function BlockRenderer(props: BlockRendererProps) {
