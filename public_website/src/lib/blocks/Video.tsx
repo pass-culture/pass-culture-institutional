29538f3e55b6c055e4e4eb86ae5f6d85e2d3c383
Amadou Affo <62760390+AffoAmadou@users.noreply.github.com>
2024-03-16 15:28:04 +0100

    tests

--- public_website/src/lib/blocks/Video.tsx
+++ public_website/src/lib/blocks/Video.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactPlayer from 'react-player/youtube'
+import ReactPlayer from 'react-player'
 import styled, { css } from 'styled-components'
 
 interface VideoProps {
