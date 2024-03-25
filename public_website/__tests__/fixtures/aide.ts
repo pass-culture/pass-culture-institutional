19bdfc3d0add7006076783a5055cde8c3e2be1c4
Amadou Affo <62760390+AffoAmadou@users.noreply.github.com>
2024-03-25 15:20:24 +0100

    fix added fixtures for page help

---
+++ public_website/__tests__/fixtures/aide.ts
@@ -0,0 +1,130 @@
+export const AidePageFixtures = {
+  helpdata: {
+    id: 1,
+    attributes: {
+      cardData: {
+        id: 1,
+        attributes: {
+          cardText: {
+            firstCta: {
+              Label: 'Voir toute la FAQ',
+              URL: '#',
+              id: 62,
+            },
+            id: 1,
+            image: {
+              data: {
+                attributes: {
+                  alternativeText: null,
+                  caption: null,
+                  createdAt: '2024-02-20T13:46:40.946Z',
+                  ext: '.jpg',
+
+                  hash: 'brooke_cagle_g1_Kr4_Ozfoac_unsplash_0448a05365',
+                  height: 3487,
+                  mime: 'image/jpeg',
+                  name: 'brooke-cagle-g1Kr4Ozfoac-unsplash.jpg',
+                  previewUrl: null,
+                  provider: 'local',
+                  provider_metadata: null,
+                  size: 3731.35,
+                  updatedAt: '2024-03-08T13:37:00.952Z',
+                  url: '/uploads/brooke_cagle_g1_Kr4_Ozfoac_unsplash_0448a05365.jpg',
+                  width: 5231,
+                },
+                id: 4,
+              },
+            },
+            secondCta: {
+              Label: 'Contacter le support',
+              URL: '#',
+              id: 63,
+            },
+            text: 'asdsadsssa',
+            title: 'Vous n’avez pas trouvé la réponse à votre question ?',
+          },
+          createdAt: '2024-03-16T14:35:17.156Z',
+          heroSection: {
+            Image: {
+              data: {
+                attributes: {
+                  alternativeText: null,
+                  caption: null,
+                  createdAt: '2024-02-27T14:42:54.536Z',
+                  ext: '.png',
+
+                  hash: 'Group_634_cbfb0ca8af',
+                  height: 1008,
+                  mime: 'image/png',
+                  name: 'Group 634.png',
+                  previewUrl: null,
+                  provider: 'local',
+                  provider_metadata: null,
+                  size: 323.2,
+                  updatedAt: '2024-03-16T14:34:18.300Z',
+                  url: '/uploads/Group_634_cbfb0ca8af.png',
+                  width: 970,
+                },
+                id: 9,
+              },
+            },
+            id: 5,
+            Surtitle: 'sdf',
+            Text: 'sdfdss',
+            Title: 'Aide et accompagnement',
+          },
+          publishedAt: '2024-03-16T14:35:21.940Z',
+          simplepushcta: [
+            {
+              cta: {
+                Label: 'Accéder à la page',
+                URL: '#',
+                id: 64,
+              },
+              id: 1,
+              image: {
+                data: {
+                  attributes: {
+                    alternativeText: null,
+                    caption: null,
+                    createdAt: '2024-02-20T13:46:41.247Z',
+                    ext: '.jpg',
+
+                    hash: 'microsoft_edge_Px0_X7g1mc8k_unsplash_1489223492',
+                    height: 6446,
+                    mime: 'image/jpeg',
+                    name: 'microsoft-edge-Px0X7g1mc8k-unsplash.jpg',
+                    previewUrl: null,
+                    provider: 'local',
+                    provider_metadata: null,
+                    size: 5112.77,
+                    updatedAt: '2024-02-26T12:28:09.419Z',
+                    url: '/uploads/microsoft_edge_Px0_X7g1mc8k_unsplash_1489223492.jpg',
+                    width: 4299,
+                  },
+                  id: 5,
+                },
+              },
+              surtititle: 'Pour aller plus loin',
+              title: 'Les offres et expériences à vivre avec le pass',
+            },
+          ],
+          social: [
+            {
+              id: 9,
+              socialMediaLink: [
+                {
+                  id: 30,
+                  name: 'youtube',
+                  url: '#',
+                },
+              ],
+              title: 'On reste connectés',
+            },
+          ],
+          updatedAt: '2024-03-16T14:35:21.944Z',
+        },
+      },
+    },
+  },
+}
