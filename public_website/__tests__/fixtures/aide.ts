b6be083d413adc06725abffb44599fccadf72a85
Amadou Affo <62760390+AffoAmadou@users.noreply.github.com>
2024-03-27 04:45:38 +0100

    fixed fixture aide

--- public_website/__tests__/fixtures/aide.ts
+++ public_website/__tests__/fixtures/aide.ts
@@ -68,7 +68,7 @@ export const AidePageFixtures = {
         id: 5,
         Surtitle: 'sdf',
         Text: 'sdfdss',
-        Title: 'Aide et accompagnement',
+        title: 'Aide et accompagnement',
       },
       publishedAt: '2024-03-16T14:35:21.940Z',
       simplepushcta: [
