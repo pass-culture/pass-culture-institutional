9717ff34a6329ff06b4efae33ca58e35635733f3
Adrien Boutigny <adrien@bien-fonde.com>
2024-04-09 12:52:02 +0200

    remove commented stuff from ContentWrapper

--- public_website/src/ui/components/ContentWrapper.tsx
+++ public_website/src/ui/components/ContentWrapper.tsx
@@ -1,22 +1,4 @@
-// import React, { ReactNode} from "react"
-
 import styled from 'styled-components'
-
-// interface ContentWrapperProps {
-//   children?: React.ReactNode
-// }
-
-// export function ContentWrapper(props: ContentWrapperProps) {
-//   return <div></div>
-// }
-
-/*
-  Figma grid:
-    12 columns of 80px
-    11 gutters of 23px (??)
-    = 80*12+23*11 = 1213px = 75,8125rem
-
-*/
 
 export const ContentWrapper = styled.div`
   margin: auto;
