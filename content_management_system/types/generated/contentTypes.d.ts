4f977c75a44152349bf3ea0431239dc016665056
Adrien Boutigny <adrien@bien-fonde.com>
2024-04-12 18:15:18 +0200

    add more blocks ot event/news/resource pages

--- content_management_system/types/generated/contentTypes.d.ts
+++ content_management_system/types/generated/contentTypes.d.ts
@@ -762,11 +762,30 @@ export interface ApiEventEvent extends Schema.CollectionType {
     slug: Attribute.String & Attribute.Required & Attribute.Unique;
     blocks: Attribute.DynamicZone<
       [
-        'block.image',
-        'block.video',
+        'block.centered-text',
+        'block.header',
         'block.social-media',
+        'block.double-push-cta',
+        'block.image-text',
+        'block.image',
+        'block.little-list',
+        'block.separator',
+        'block.simple-push-cta',
+        'block.space',
+        'block.vertical-carousel',
+        'block.video',
+        'block.key-number-carousel',
+        'block.logos',
+        'block.latest-news',
+        'block.experience-video-carousel',
+        'block.offers-carousel',
+        'block.piled-cards',
+        'block.faq',
+        'block.detailed-logos',
+        'block.organization-chart',
         'block.simple-text-v2',
-        'block.double-push-cta'
+        'block.image-gallery',
+        'block.breadcrumb'
       ]
     >;
     seo: Attribute.Component<'shared.seo'>;
@@ -1189,11 +1208,30 @@ export interface ApiNewsNews extends Schema.CollectionType {
       Attribute.Required;
     blocks: Attribute.DynamicZone<
       [
-        'block.image',
-        'block.video',
-        'block.simple-text-v2',
+        'block.centered-text',
+        'block.header',
+        'block.social-media',
         'block.double-push-cta',
-        'block.social-media'
+        'block.image-text',
+        'block.image',
+        'block.little-list',
+        'block.separator',
+        'block.simple-push-cta',
+        'block.space',
+        'block.vertical-carousel',
+        'block.video',
+        'block.key-number-carousel',
+        'block.logos',
+        'block.latest-news',
+        'block.experience-video-carousel',
+        'block.offers-carousel',
+        'block.piled-cards',
+        'block.faq',
+        'block.detailed-logos',
+        'block.organization-chart',
+        'block.simple-text-v2',
+        'block.image-gallery',
+        'block.breadcrumb'
       ]
     >;
     seo: Attribute.Component<'shared.seo'>;
@@ -1415,11 +1453,30 @@ export interface ApiResourceResource extends Schema.CollectionType {
       Attribute.Required;
     blocks: Attribute.DynamicZone<
       [
-        'block.image',
-        'block.simple-text-v2',
-        'block.video',
+        'block.centered-text',
+        'block.header',
+        'block.social-media',
         'block.double-push-cta',
-        'block.social-media'
+        'block.image-text',
+        'block.image',
+        'block.little-list',
+        'block.separator',
+        'block.simple-push-cta',
+        'block.space',
+        'block.vertical-carousel',
+        'block.video',
+        'block.key-number-carousel',
+        'block.logos',
+        'block.latest-news',
+        'block.experience-video-carousel',
+        'block.offers-carousel',
+        'block.piled-cards',
+        'block.faq',
+        'block.detailed-logos',
+        'block.organization-chart',
+        'block.simple-text-v2',
+        'block.image-gallery',
+        'block.breadcrumb'
       ]
     >;
     partnership: Attribute.Enumeration<
