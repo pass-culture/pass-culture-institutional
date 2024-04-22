5ee43a5ddad15f4a0891a27a8b798b3f3241bc72
LucasBeneston <benestonlucas@hotmail.fr>
2024-04-22 11:58:08 +0200

    (PC-29263) refactor(text): remove style in parseTitleWithMarkup

--- public_website/src/utils/parseTitleWithMarkup.tsx
+++ public_website/src/utils/parseTitleWithMarkup.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import styled, { css } from 'styled-components'
 
 export const parseTitleWithMarkup = (
   text: string
@@ -9,7 +8,7 @@ export const parseTitleWithMarkup = (
 
   const textWithMarkup = parts.map((part, index) => {
     if (index % 2 === 1) {
-      return <HighlightedMark key={index}>{part}</HighlightedMark>
+      return <mark key={index}>{part}</mark>
     } else {
       return <span key={index}>{part}</span>
     }
@@ -17,18 +16,3 @@ export const parseTitleWithMarkup = (
 
   return { textWithMarkup, accessibilityLabel }
 }
-
-const HighlightedMark = styled.mark`
-  ${({ theme }) => css`
-    background: none;
-    background-image: linear-gradient(
-      to right,
-      ${theme.colors.flashGreen} 50%,
-      ${theme.colors.flashGreen} 50%
-    );
-    background-size: 200% 0.4em;
-    background-position: 100% 90%;
-    background-repeat: no-repeat;
-    color: inherit;
-  `}
-`
