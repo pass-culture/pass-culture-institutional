eb0cf57fd117d24a865472f04ec0cd57fa61fe31
lheneman-pass <laurent.heneman@passculture-ext.app>
2024-06-25 10:09:21 +0200

    fix: fix sur parseText et stripTags (#384)
    Co-authored-by: PassCulture <passculture@macstudelaurent.home>

--- public_website/src/utils/stripTags.ts
+++ public_website/src/utils/stripTags.ts
@@ -1,9 +1,13 @@
 /**
  * Remove HTML tags from a string and merge spaces
  */
-export function stripTags(string: string): string {
-  return string
-    .replace(/(<([^>]+)>)/gi, '')
-    .replace(/\s\s+/g, ' ')
-    .replace('&nbsp;', ' ')
+export function stripTags(str: string): string {
+  if (str) {
+    return str
+      .replace(/(<([^>]+)>)/gi, '')
+      .replace(/\s\s+/g, ' ')
+      .replace('&nbsp;', ' ')
+  } else {
+    return ''
+  }
 }
