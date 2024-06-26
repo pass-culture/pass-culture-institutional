6e17df9218027ae9d71b790752fac40c74661402
PassCulture <passculture@macstudelaurent.home>
2024-06-26 08:39:23 +0200

    refactor: refactor filterByAttribute

--- public_website/src/utils/filterbyAttributes.ts
+++ public_website/src/utils/filterbyAttributes.ts
@@ -1,53 +1,35 @@
 import { APIResponseData } from '@/types/strapi'
 
-type Filtre =
-  | ({ id: number } & Omit<object & { filtre: string }, never>[])
-  | undefined
+type Filtre = { id: number; filtre: string }[] | undefined
 
 type Data =
   | APIResponseData<'api::event.event'>[]
   | APIResponseData<'api::news.news'>[]
-
+type AttributeGetter = (
+  item: APIResponseData<'api::event.event'> | APIResponseData<'api::news.news'>
+) => string
+const filterMappings: { [key: string]: AttributeGetter } = {
+  "Secteur d'activités": (item) => item.attributes.secteur,
+  // eslint-disable-next-line prettier/prettier
+  "Catégorie": (item) => item.attributes.category,
+  // eslint-disable-next-line prettier/prettier
+  "Localisation": (item) => item.attributes.localisation,
+}
 export const filterByAttribute = (filtres: Filtre, data: Data) => {
+  if (!filtres) return undefined
+
   const _filtres = structuredClone(filtres)
   const _data = structuredClone(data)
 
-  const uniqueValue = <
-    T extends
-      | APIResponseData<'api::event.event'>
-      | APIResponseData<'api::news.news'>,
-  >(
-    data: T[],
-    attribute: (item: T) => string
+  const getUniqueValues = (
+    attribute: (item: (typeof _data)[0]) => string
   ): string[] => {
-    return Array.from(new Set(data.map(attribute)))
+    return Array.from(new Set(_data.map(attribute)))
   }
 
-  return _filtres?.map((filtre) => {
-    let uniqueValues: string[] = []
-
-    switch (filtre.filtre) {
-      case "Secteur d'activités":
-        uniqueValues = uniqueValue(
-          _data as APIResponseData<'api::event.event'>[],
-          (item) => item.attributes.secteur
-        )
-        break
-      case 'Catégorie':
-        uniqueValues = uniqueValue(
-          _data as APIResponseData<'api::news.news'>[],
-          (item) => item.attributes.category
-        )
-        break
-      case 'Localisation':
-        uniqueValues = uniqueValue(
-          _data as APIResponseData<'api::news.news'>[],
-          (item) => item.attributes.localisation
-        )
-        break
-      default:
-        break
-    }
+  return _filtres.map((filtre) => {
+    const attributeGetter = filterMappings[filtre.filtre]
+    const uniqueValues = attributeGetter ? getUniqueValues(attributeGetter) : []
 
     return {
       ...filtre,
