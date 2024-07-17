f6055a6eee1bb7cef7a2df4f60a098dec8459089
lheneman-pass <laurent.heneman@passculture-ext.app>
2024-07-17 11:43:38 +0200

    fix: fil arianne mal placé (#411)
    Co-authored-by: PassCulture <passculture@macstudelaurent.home>

--- public_website/src/pages/_document.tsx
+++ public_website/src/pages/_document.tsx
@@ -32,7 +32,7 @@ export default class MyDocument extends Document {
 
   render() {
     return (
-      <Html lang="en">
+      <Html lang="fr">
         <Head>
           <link rel="manifest" href="/site.webmanifest" />
           <link
