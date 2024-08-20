158edbdccdad9b77dc4fc9cbccbb88d23d3d5e7e
Christopher Gerrard <vikingprank@outlook.com>
2024-08-20 12:02:50 +0200

    Update public_website/src/hooks/useAxeptio.ts (add slim sdk)
    Co-authored-by: Lucas BENESTON <62059034+Lucasbeneston@users.noreply.github.com>

--- public_website/src/hooks/useAxeptio.ts
+++ public_website/src/hooks/useAxeptio.ts
@@ -8,7 +8,7 @@ export const useAxeptio = () => {
       window.axeptioSettings = { clientId: "66bcc4aa49e3d3d814d9f71b", cookiesVersion: "siteinstit testing-fr-EU", };
       (function(d, s) {
         var t = d.getElementsByTagName(s)[0], e = d.createElement(s);
-        e.async = true; e.src = "//static.axept.io/sdk.js";
+        e.async = true; e.src = "//static.axept.io/sdk-slim.js";
         t.parentNode.insertBefore(e, t);
       })(document, "script");
     `
