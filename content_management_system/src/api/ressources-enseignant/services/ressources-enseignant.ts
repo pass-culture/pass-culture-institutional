73632a26240093209bba56d0692e3035803710d2
LucasBeneston <lucas.beneston@passculture.app>
2024-09-12 21:18:02 +0200

    feat: remove unused ressources-enseignant

--- content_management_system/src/api/ressources-enseignant/services/ressources-enseignant.ts
+++
@@ -1,7 +0,0 @@
-/**
- * ressources-enseignant service
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreService('api::ressources-enseignant.ressources-enseignant');
