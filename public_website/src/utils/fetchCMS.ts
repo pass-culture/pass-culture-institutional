fafbbd9fc624740fa0e01f822e14d92e1412fbc0
Lucas BENESTON <62059034+Lucasbeneston@users.noreply.github.com>
2024-09-16 09:22:09 +0200

    feat: try to fix preview mode (#493)

--- public_website/src/utils/fetchCMS.ts
+++ public_website/src/utils/fetchCMS.ts
@@ -5,27 +5,17 @@ type HttpResponse<T> = {
 }
 export async function fetchCMS<T>(path: string) {
   try {
-    const isDevMode = process.env.NODE_ENV === 'development'
     const apiPath = `/api${path}`
-    const strapiURL = getStrapiURL(apiPath)
-    const isLocalHost = strapiURL.includes('localhost')
-    const sep = strapiURL.includes('?') ? '&' : '?'
-    const requestUrl = `${strapiURL}${
+    const sep = getStrapiURL(apiPath).includes('?') ? '&' : '?'
+    const requestUrl = `${getStrapiURL(apiPath)}${
       process.env['NEXT_PUBLIC_PREVIEW_MODE'] === 'true'
         ? `${sep}publicationState=preview`
         : ''
     }`
 
-    if (isDevMode && !isLocalHost && !process.env['NEXT_PUBLIC_IAP_ID_TOKEN']) {
-      throw new Error(
-        `To use the remote CMS, you need NEXT_PUBLIC_IAP_ID_TOKEN to be present on your machine.`
-      )
-    }
-
     const mergedOptions = {
       headers: {
         'Content-Type': 'application/json',
-        Authorization: `Bearer ${process.env['NEXT_PUBLIC_IAP_ID_TOKEN']}`,
       },
     }
 
