b67d4c2c10122d19c2be6c5f05ee0fda5f88b204
Christopher Gerrard <christopher.gerrard@passculture.app>
2024-10-21 18:19:00 +0200

    Removed function from hook to avoid rerendering it
    Co-authored-by: bruno ebstein <bruno-ebstein-pass-culture@users.noreply.github.com>

--- public_website/src/hooks/useConsent.ts
+++ public_website/src/hooks/useConsent.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { Dispatch, SetStateAction, useEffect, useState } from 'react'
 
 declare global {
   interface Window {
@@ -6,30 +6,31 @@ declare global {
   }
 }
 
+const getAcceptedVendors = (
+  setAcceptedVendors: Dispatch<SetStateAction<Record<string, boolean>>>
+) => {
+  window._axcb = window._axcb || []
+  window._axcb.push(function (sdk: unknown) {
+    if (
+      typeof sdk === 'object' &&
+      sdk !== null &&
+      'hasAcceptedVendor' in sdk &&
+      typeof sdk['hasAcceptedVendor'] === 'function'
+    ) {
+      setAcceptedVendors({
+        firebase: sdk.hasAcceptedVendor('googlefirebase'),
+      })
+    }
+  })
+}
+
 export const useConsent = () => {
   const [acceptedVendors, setAcceptedVendors] = useState<
     Record<string, boolean>
   >({ firebase: false })
 
-  const getAcceptedVendors = async () => {
-    window._axcb = window._axcb || []
-    window._axcb.push(function (sdk: unknown) {
-      if (
-        typeof sdk === 'object' &&
-        sdk !== null &&
-        'hasAcceptedVendor' in sdk
-      ) {
-        setAcceptedVendors({
-          firebase: (
-            sdk as { hasAcceptedVendor: (vendor: string) => boolean }
-          ).hasAcceptedVendor('googlefirebase'),
-        })
-      }
-    })
-  }
-
   useEffect(() => {
-    getAcceptedVendors()
+    getAcceptedVendors(setAcceptedVendors)
   }, [])
 
   return acceptedVendors
