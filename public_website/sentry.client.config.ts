018754ce72c449b234116ef21bab8cebc66a77cb
Xavier De Koninck <167757543+xdekoninck-pass@users.noreply.github.com>
2024-12-23 12:04:14 +0100

    perf(monitoring): reduce sentry sampling rates to optimize costs

--- public_website/sentry.client.config.ts
+++ public_website/sentry.client.config.ts
@@ -5,10 +5,10 @@ import * as Sentry from '@sentry/nextjs'
 
 Sentry.init({
   dsn: 'https://243b7abda6774f78805e4684f77c6d9d@sentry.passculture.team/12',
-  tracesSampleRate: 1,
+  tracesSampleRate: 0.1,
   debug: false,
-  replaysOnErrorSampleRate: 1.0,
-  replaysSessionSampleRate: 1.0,
+  replaysSessionSampleRate: 0.0,
+  replaysOnErrorSampleRate: 0.1,
   integrations: [
     Sentry.replayIntegration({
       maskAllText: true,
