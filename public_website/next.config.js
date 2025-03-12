ca7aba717944f685d9f3ad46b45a517d054c0123
xdekoninck-pass <167757543+xdekoninck-pass@users.noreply.github.com>
2025-03-12 10:53:04 +0100

    feat(sentry): completely remove sentry (#565)

--- public_website/next.config.js
+++ public_website/next.config.js
@@ -12,27 +12,3 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
-
-/* eslint-disable @typescript-eslint/no-var-requires */
-const { withSentryConfig } = require('@sentry/nextjs')
-
-module.exports = withSentryConfig(
-  module.exports,
-  {
-    // For all available options, see:
-    // https://github.com/getsentry/sentry-webpack-plugin#options
-    silent: true,
-    org: 'sentry',
-    project: 'institutional',
-    url: 'https://sentry.passculture.team/',
-  },
-  {
-    // For all available options, see:
-    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-    widenClientFileUpload: true,
-    transpileClientSDK: true,
-    hideSourceMaps: true,
-    disableLogger: true,
-    automaticVercelMonitors: true,
-  }
-)
--- public_website/src/pages/_error.tsx
+++ public_website/src/pages/_error.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import * as Sentry from '@sentry/nextjs'
 import type { NextPage } from 'next'
 import type { ErrorProps } from 'next/error'
 import Error from 'next/error'
@@ -12,10 +11,6 @@ const CustomErrorComponent: NextPage<ErrorProps> = ({ statusCode }) => {
 CustomErrorComponent.propTypes = { statusCode: PropTypes.number.isRequired }
 
 CustomErrorComponent.getInitialProps = async (contextData) => {
-  // In case this is running in a serverless function, await this in order to give Sentry
-  // time to send the error before the lambda exits
-  await Sentry.captureUnderscoreErrorException(contextData)
-
   // This will contain the status code of the response
   return Error.getInitialProps(contextData)
 }
